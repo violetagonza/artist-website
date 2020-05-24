a2ee7499a875c0877ec9d7748f85c3b2c7ab31bb
violetagonza <violetagonzalezcalleja@gmail.com>
2020-05-24 20:21:07 +0200

    Add styles to Arcana and some general styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,20 +7,24 @@ function Bio() {
   return (
     <>
       <Link to='/' title='volver'>
-        <i className='fas fa-long-arrow-alt-left'></i>
+        <i className='fas fa-long-arrow-alt-left arrow-icon'></i>
       </Link>
       <Header title='Bio' />
       <main>
-        <article>
-          <p>Tania was baptized with three female names, as they used to do. It was in the neighborhood of El Carmel, in Barcelona, in the parish dedicated Saint Theresa of Jesus, the woman that knew how to explain that God "moved among the cooking pots,” a declaration which, even when expressed in devout christian terms, epitomizes the first great lesson of the feminist economy: domestic duties and knowledges dedicated to maintaining life have a fundamental value. In other words: the personal is political.</p>
-          <p>This symbolic relationship serves to weave the threads between Tania Berta Judith’s field of work and artistic experimentation: the restoration of the histories of women, who so often disappear in historiography. Her practice moves from her personal experience and own genealogy to the reinterpretation of history and the role of women and their knowledges in their tailoring of the world.</p>
-          <p>Tania’s work is radically feminist. In it, embroidery becomes a politicized technique that is used to reclaim the knowledges of women, especially those that have been passed on via the maternal line, restoring authority to this practice as a space for the transmission of knowledges.</p>
-          <p>In the face of culture and the written word, crucial technology for the construction of patriarchal power and the normalization of a world explained through androcentrism, Tania Berta Judith stirs the hierarchy of the illustrated knowledges and designs a symbolic world where orality acquires power, intuition becomes method, and the needle embroiders other holy scriptures.</p>
+        <article className='bio'>
+          <div>
+            <p className='text'>Tania was baptized with three female names, as they used to do. It was in the neighborhood of El Carmel, in Barcelona, in the parish dedicated Saint Theresa of Jesus, the woman that knew how to explain that God "moved among the cooking pots,” a declaration which, even when expressed in devout christian terms, epitomizes the first great lesson of the feminist economy: domestic duties and knowledges dedicated to maintaining life have a fundamental value. In other words: the personal is political.</p>
+            <p className='text'>This symbolic relationship serves to weave the threads between Tania Berta Judith’s field of work and artistic experimentation: the restoration of the histories of women, who so often disappear in historiography. Her practice moves from her personal experience and own genealogy to the reinterpretation of history and the role of women and their knowledges in their tailoring of the world.</p>
+            <p className='text'>Tania’s work is radically feminist. In it, embroidery becomes a politicized technique that is used to reclaim the knowledges of women, especially those that have been passed on via the maternal line, restoring authority to this practice as a space for the transmission of knowledges.</p>
+            <p className='text'>In the face of culture and the written word, crucial technology for the construction of patriarchal power and the normalization of a world explained through androcentrism, Tania Berta Judith stirs the hierarchy of the illustrated knowledges and designs a symbolic world where orality acquires power, intuition becomes method, and the needle embroiders other holy scriptures.</p>
+            <footer>
+              <p className='text signature'>Mar Cianuro</p>
+            </footer>
+          </div>
+          <div>
+            <img className='bio-img' alt='Retrato de Tania Berta Judith' src={image}></img>
+          </div>
         </article>
-        <footer>
-          <p>Mar Cianuro</p>
-        </footer>
-        <img alt='Retrato de Tania Berta Judith' src={image}></img>
       </main>
     </>
   );
