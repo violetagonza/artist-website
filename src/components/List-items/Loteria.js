daac5562acdc82f8ac1d03a81dceded0d5aca89f
violetagonza <violetagonzalezcalleja@gmail.com>
2020-05-24 15:04:22 +0200

    Complete Loteria component's content and structure

--- src/components/List-items/Loteria.js
+++ src/components/List-items/Loteria.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../Header';
+import loteria1 from '../../images/loteria/loteria_1.png';
+import loteria2 from '../../images/loteria/loteria_2.png';
+import loteria3 from '../../images/loteria/loteria_3.png';
 import { Link } from 'react-router-dom';
 
 function Loteria() {
@@ -9,7 +12,12 @@ function Loteria() {
         <i className='fas fa-long-arrow-alt-left'></i>
       </Link>
       <Header title='Lotería de la mama 23046/32046' />
-      <main></main>
+      <main>
+        <p>Sábana heredada con un roto en forma de “siete” en donde transfiero a bordado la nota que lleva mi abuela en la cartera con los números de la lotería a los que jugaba su madre. </p>
+      </main>
+      <img src={loteria1} alt='' />
+      <img src={loteria2} alt='' />
+      <img src={loteria3} alt='' />
     </>
   );
 }
