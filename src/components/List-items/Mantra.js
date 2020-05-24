8f4d671ea96d7825155054a53a857764f836135a
violetagonza <violetagonzalezcalleja@gmail.com>
2020-05-24 14:59:39 +0200

    Complete Mantra component's content and structure

--- src/components/List-items/Mantra.js
+++ src/components/List-items/Mantra.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from '../Header';
+import mantra1 from '../../images/mantra/mantra_1.png';
+import mantra2 from '../../images/mantra/mantra_2.png';
+import mantra3 from '../../images/mantra/mantra_3.png';
 import { Link } from 'react-router-dom';
 
 function Mantra() {
@@ -8,11 +11,13 @@ function Mantra() {
       <Link to='/' title='volver'>
         <i className='fas fa-long-arrow-alt-left'></i>
       </Link>
-      <Header
-        title='Esto es todo lo que tengo que hacer para solucionar este problema que tengo en mi mente
-'
-      />
-      <main></main>
+      <Header title='Esto es todo lo que tengo que hacer para solucionar este problema que tengo en mi mente' />
+      <main>
+        <p>Sábana heredada en donde bordo, a modo de mantra, una nota encontrada post-mortem de mi abuela política Julia: “Esto es todo lo que tengo que hacer para solucionar este problema que tengo en mi mente”. </p>
+        <img src={mantra1} alt='' />
+        <img src={mantra2} alt='' />
+        <img src={mantra3} alt='' />
+      </main>
     </>
   );
 }
